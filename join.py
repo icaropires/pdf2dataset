57e630ff8dc87d502fe37fcdab5c2eba178fa79a
Ícaro Pires <icaropsa@gmail.com>
2020-04-27 23:31:09 -0300

    Small fix to join.py

--- join.py
+++ join.py
@@ -6,11 +6,13 @@ from concurrent.futures import ProcessPoolExecutor
 
 FOLDER_PATH = Path('/tmp/pdfs/')
 
+
 def get_text(path):
     with path.open() as f:
         text = f.read()
 
     return path, text
+
 
 def search_texts():
     text_files = list(FOLDER_PATH.glob('**/*.txt'))
@@ -20,15 +22,16 @@ def search_texts():
         with tqdm(total=len(text_files), desc='Reading files...') as pbar:
             futures = []
             for text_file in text_files:
-               future = executor.submit(get_text, text_file) 
-               future.add_done_callback(lambda p: pbar.update())
-               futures.append(future)
+                future = executor.submit(get_text, text_file)
+                future.add_done_callback(lambda p: pbar.update())
+                futures.append(future)
 
             for future in futures:
                 path, text = future.result()
                 texts[path.resolve()] = text
 
     return texts
+
 
 def get_texts():
     pickle_file = Path('texts.pickle')
@@ -37,8 +40,9 @@ def get_texts():
         print('Searching for texts...')
         texts = search_texts()
 
+        print('Saving pickle with the result...')
         with pickle_file.open('wb') as f:
-            pickle.dump(texts, f) 
+            pickle.dump(texts, f)
 
         print(f'New {pickle_file} saved!')
     else:
