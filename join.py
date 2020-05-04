49632bff991ef76d72c882016e8e4fe2e83acfe4
Ícaro Pires <icaropsa@gmail.com>
2020-05-04 01:04:53 +0000

    Make join.py join errors files

--- join.py
+++ join.py
@@ -5,10 +5,12 @@ from pathlib import Path
 from tqdm import tqdm
 
 
-def read_texts(dir_path, *, workers=min(32, os.cpu_count() + 4)):
+def read_texts(dir_path, *,
+               workers=min(32, os.cpu_count() + 4),
+               pattern='*.txt'):
 
     dir_path = Path(dir_path)
-    text_files = list(dir_path.rglob('*.txt'))
+    text_files = list(dir_path.rglob(pattern))
 
     with ProcessPoolExecutor(max_workers=workers) as executor:
         with tqdm(total=len(text_files),
@@ -26,25 +28,36 @@ def read_texts(dir_path, *, workers=min(32, os.cpu_count() + 4)):
     return texts
 
 
-def getandsave_texts(dir_path, *, pickle_file='texts.pickle'):
-    pickle_file = Path(pickle_file)
+def join(dir_path, *,
+         results_file='texts.pickle',
+         error_file='errors.log',
+         workers=min(32, os.cpu_count() + 4),
+         error_suffix='_error.log'):
 
-    if pickle_file.exists():
-        print(f"'{pickle_file}' exists, using it...")
-        texts = pickle.loads(pickle_file.read_bytes())
+    results_file = Path(results_file)
+    error_file = Path(error_file)
+
+    if results_file.exists():
+        print(f"'{results_file}' exists, using it...")
+        texts = pickle.loads(results_file.read_bytes())
         return texts
 
     print('Searching for texts...')
-    texts = read_texts(dir_path)
+    texts = read_texts(dir_path, workers=workers)
 
-    print('Saving pickle with the result...')
-    pickle_file.write_bytes(pickle.dumps(texts))
-    print(f"New '{pickle_file}' saved!")
+    error_pat = f'*{error_suffix}'
+    errors = read_texts(dir_path, pattern=error_pat, workers=workers)
+    errors = '\n\n'.join(f'{path}: {error}' for path, error in errors.items())
+
+    print('Saving files...')
+    results_file.write_bytes(pickle.dumps(texts))
+    error_file.write_text(errors)
+    print(f"New '{results_file}' and '{error_file}' saved!")
 
     return texts
 
 
 if __name__ == '__main__':
-    texts = getandsave_texts('txts')
+    texts = join('/tmp/text')
     print('Texts available')
     # TODO: continue...
