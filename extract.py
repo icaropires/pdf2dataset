c489c6197c08a6b283fe8c98e954a200c89da933
Ícaro Pires <icaropsa@gmail.com>
2020-05-22 20:05:33 -0300

    Use chunks for processing pages and add use of ray

--- extract.py
+++ extract.py
@@ -2,17 +2,15 @@
 
 import argparse
 import os
-from concurrent.futures import ProcessPoolExecutor, as_completed
+import traceback
 from pathlib import Path
 
 import cv2
 import numpy as np
 import pytesseract
-from pdf2image import (
-    convert_from_path,
-    pdfinfo_from_path,
-)
-from pdf2image.exceptions import PDFPageCountError
+from pdf2image import convert_from_path, pdfinfo_from_path
+from pdf2image.exceptions import PDFPageCountError, PDFSyntaxError
+from ray.util.multiprocessing import Pool
 from tqdm import tqdm
 
 
@@ -58,10 +56,15 @@ class ExtractionTask:
         return img[0]
 
     def process(self):
-        img = self.get_page_img()
-        text = self.ocr_image(img)
+        text, error = None, None
 
-        return text
+        try:
+            img = self.get_page_img()
+            text = self.ocr_image(img)
+        except (PDFPageCountError, PDFSyntaxError):
+            error = traceback.format_exc()
+
+        return text, error
 
 
 class TextExtraction:
@@ -73,21 +76,38 @@ class TextExtraction:
         self.max_workers = max_workers
 
     @staticmethod
-    def generate_tasks(docs):
+    def _list_pages(d):
+        try:
+            num_pages = pdfinfo_from_path(d)['Pages']
+            pages = range(1, num_pages+1)
+        except PDFPageCountError:
+            pages = [-1]  # Handled when processing
+
+        return pages
+
+    def gen_tasks(self, docs):
         '''
         Returns tasks to be processed.
         For faulty documents, only the page -1 will be available
         '''
-        def list_pages(d):
-            try:
-                num_pages = pdfinfo_from_path(d)['Pages']
-                pages = range(1, num_pages+1)
-            except PDFPageCountError:
-                pages = [-1]  # Handled when processing
+        # 10 is not a mistake, is because this is a fast operation
+        chunksize = max(1, (len(docs)/self.max_workers)//10)
 
-            return pages
+        print()
+        tasks = []
+        with tqdm(desc='Generating tasks', unit='tasks') as pbar:
+            with Pool() as pool:
+                results = pool.imap(
+                    self._list_pages, docs, chunksize=chunksize
+                )
+                results = zip(docs, results)
 
-        return (ExtractionTask(d, p) for d in docs for p in list_pages(d))
+                for doc, range_pages in results:
+                    new_tasks = [ExtractionTask(doc, p) for p in range_pages]
+                    tasks += new_tasks
+                    pbar.update(len(new_tasks))
+
+        return tasks
 
     @staticmethod
     def get_docs(input_dir):
@@ -103,12 +123,8 @@ class TextExtraction:
 
         return out_path
 
-    def _get_result(self, task, future):
-        result = None
-
-        try:
-            result = future.result()
-        except Exception as e:
+    def save_result(self, result, task, error):
+        if error is not None:
             page = task.page if task.page != -1 else 'doc'
             error_suffix = f'_{page}_error.log'
 
@@ -116,40 +132,44 @@ class TextExtraction:
             name = output_path.stem + error_suffix
 
             error_file = output_path.with_name(name)
-            error_file.write_text(str(e))
+            error_file.write_text(error)
 
-        return result
-
-    def save_result(self, result, task):
-        if result is not None:
+        elif result is not None:
             output_path = self.get_output_path(task.doc)
             name = f'{output_path.stem}_{task.page}.txt'
 
             results_file = output_path.with_name(name)
             results_file.write_text(result)
 
+    @staticmethod
+    def _process_task(task):
+        result, error = task.process()
+        return task, result, error
+
     def apply(self):
         pdf_files = self.get_docs(input_dir)
-        tasks = list(self.generate_tasks(pdf_files))
+        tasks = self.gen_tasks(pdf_files)
 
-        print(f'\nPDFs directory: {self.input_dir}'
-              f'\nOutput directory: {self.output_dir}'
-              f'\nUsing {self.max_workers} workers', end='\n\n')
+        chunksize = max(1, (len(tasks)/self.max_workers)//100)
 
-        with ProcessPoolExecutor(max_workers=self.max_workers) as ex:
-            with tqdm(total=len(tasks), desc='Processing pages',
-                      unit='pages', dynamic_ncols=True) as pbar:
-                def submit(task):
-                    future = ex.submit(task.process)
-                    future.add_done_callback(lambda _: pbar.update())
+        print(f'\nPDFs directory: {self.input_dir}',
+              f'Output directory: {self.output_dir}',
+              f'Using {self.max_workers} workers',
+              f'Chunksize: {chunksize}', sep='\n', end='\n\n')
 
-                    return future
+        def get_bar(results):
+            return tqdm(
+                results, total=len(tasks), desc='Processing pages',
+                unit='pages', dynamic_ncols=True
+            )
 
-                fs_to_tasks = {submit(t): t for t in tasks}
-                for f in as_completed(fs_to_tasks):
-                    task = fs_to_tasks[f]
-                    result = self._get_result(task, f)
-                    self.save_result(result, task)  # TODO: parallelize
+        with Pool() as pool:
+            results = pool.imap_unordered(
+                self._process_task, tasks, chunksize=chunksize
+            )
+
+            for task, result, error in get_bar(results):
+                self.save_result(result, task, error)  # TODO: threads, async
 
 
 if __name__ == '__main__':
